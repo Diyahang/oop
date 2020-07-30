ce550c4796d4bf51a120393b9edc4989f5e9e9c0
unknown <anggrainidiyah25@gmail.com>
2020-07-30 13:12:18 +0700

    inharitance

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//class
+class Animal {
+  //property
+  public $legs = 2;
+  public $cold_blooded = false;
+
+  public function __construct($name){
+    $this->name = $name;
+  }
+  function getName(){
+    echo $this->name;
+  }
+  function get_cold_blooded(){
+    echo $this->cold_blooded;
+  }
+}
+
+
+
+ ?>
